4d7005a6af41ca489ce33a0de19f482c647480d3
马龙 <malong@xforceplus.com>
2021-09-08 17:44:07 +0800

    init

---
+++ wapp-service/src/main/java/com/xforceplus/wapp/WappApplication.java
@@ -0,0 +1,14 @@
+package com.xforceplus.wapp;
+
+/**
+ * @author malong@xforceplus.com
+ * @program wapp-enhance
+ * @description
+ * @create 2021-09-08 17:25
+ **/
+
+public class WappApplication {
+    public static void main(String[] args) {
+
+    }
+}
