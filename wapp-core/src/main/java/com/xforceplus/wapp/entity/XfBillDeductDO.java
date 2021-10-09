e318c41e9ce449deb7fce50e23af3cbc7b5a1d67
lisongqiu <lisongqiu@xforceplus.com>
2021-10-09 18:13:37 +0800

    mybatis-plus

---
+++ wapp-core/src/main/java/com/xforceplus/wapp/entity/XfBillDeductDO.java
@@ -0,0 +1,13 @@
+package com.xforceplus.wapp.entity;
+
+import lombok.Data;
+
+@Data
+public class XfBillDeductDO {
+
+    private Long id;
+
+    private String businessNo;
+
+    private Integer businessType;
+}
---
+++ wapp-core/src/test/java/com/xforceplus/wapp/service/XfBillDeductServiceTest.java
@@ -0,0 +1,28 @@
+package com.xforceplus.wapp.service;
+
+import com.alibaba.fastjson.JSON;
+import com.xforceplus.wapp.WappApplication;
+import com.xforceplus.wapp.entity.XfBillDeductDO;
+import com.xforceplus.wapp.mapper.XfBillDeductMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = WappApplication.class)
+@Slf4j
+public class XfBillDeductServiceTest {
+    @Autowired
+    private XfBillDeductMapper xfBillDeductMapper;
+
+    @Test
+    public void testXfBillDeductList(){
+        List<XfBillDeductDO> list = xfBillDeductMapper.selectList();
+        log.info("测试==="+JSON.toJSONString(list));
+    }
+}
