ed72810aea0f8c056d5be76d9ee2b3384bcfbdca
马少鹏 <mashaopeng@xforceplus.com>
2021-10-11 15:21:08 +0800

    修改结果泛型，统一mybatis plus版本

--- wapp-common/src/main/java/com/xforceplus/wapp/common/dto/R.java
+++ wapp-common/src/main/java/com/xforceplus/wapp/common/dto/R.java
@@ -10,45 +10,45 @@ import org.apache.commons.lang3.StringUtils;
  * @create 2021-10-09 15:09
  **/
 @Data
-public class R {
+public class R<T> {
 
     public static final String OK = "XFWAPP0000";
     public static final String FAIL = "XFWAPP0000";
 
-    private Object result;
+    private T result;
     private String message;
     private String code;
 
     public R(){}
 
-    public R(Object result, String message, String code) {
+    public R(T result, String message, String code) {
         this.result = result;
         this.message = message;
         this.code = code;
     }
 
-    public static R ok(Object result) {
-        R r = new R();
+    public static <T> R<T> ok(T result) {
+        R<T> r = new R<>();
         r.code = OK;
         r.result = result;
         return r;
     }
 
-    public static R ok(Object result, String message) {
-        R r = ok(result);
+    public static <T> R<T> ok(T result, String message) {
+        R<T> r = ok(result);
         r.message = message;
         return r;
     }
 
-    public static R fail(String message) {
-        R r = new R();
+    public static  R<?> fail(String message) {
+        R<?> r = new R<>();
         r.code = FAIL;
         r.message = message;
         return r;
     }
 
-    public static R fail(String message, String code) {
-        R r = new R();
+    public static  R<?> fail(String message, String code) {
+        R<?> r = new R<>();
         r.message = message;
         r.code = StringUtils.isBlank(code) ? FAIL : code;
         return r;
