c5134145b891ed2719fd67ea5a7e910ea659030a
马少鹏 <mashaopeng@xforceplus.com>
2021-10-11 15:52:53 +0800

    修改报错包引入

--- wapp-core/src/main/java/com/xforceplus/wapp/config/MybatisPlusConfig.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/config/MybatisPlusConfig.java
@@ -1,6 +1,6 @@
 package com.xforceplus.wapp.config;
 
-import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
