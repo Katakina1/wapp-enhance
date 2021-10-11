03c1c9ffab64f44d8ed810311f1a503346a19a23
马龙 <malong@xforceplus.com>
2021-10-11 16:06:24 +0800

    单元测试调整

--- wapp-core/src/test/java/com/xforceplus/wapp/service/XfBillDeductServiceTest.java
+++ wapp-core/src/test/java/com/xforceplus/wapp/service/XfBillDeductServiceTest.java
@@ -7,20 +7,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = WappApplication.class)
 @Slf4j
+@ActiveProfiles("unit")
 public class XfBillDeductServiceTest {
 
     @Autowired
     private TXBillDeductDao xfBillDeductDao;
 
     @Test
-    public void testXfBillDeductList(){
-       // List<XfBillDeductEntity> list = xfBillDeductDao.selectList();
-       // log.info("测试==="+JSON.toJSONString(list));
+    public void testXfBillDeductList() {
+        // List<XfBillDeductEntity> list = xfBillDeductDao.selectList();
+        // log.info("测试==="+JSON.toJSONString(list));
     }
 }
