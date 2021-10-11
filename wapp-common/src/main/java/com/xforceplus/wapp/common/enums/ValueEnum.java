f28514551a062ccbb0ad2ff0e79f4263ca30a10c
杨申 <yangshen@xforceplus.com>
2021-10-11 14:46:23 +0800

    添加枚举类

---
+++ wapp-common/src/main/java/com/xforceplus/wapp/common/enums/ValueEnum.java
@@ -0,0 +1,32 @@
+package com.xforceplus.wapp.common.enums;
+
+import lombok.NonNull;
+import org.apache.commons.lang3.EnumUtils;
+
+import java.util.Optional;
+
+public interface ValueEnum<T> {
+    T getValue();
+
+    static <E extends Enum<E> & ValueEnum<T>, T> boolean isValid(Class<E> clazz, T value) {
+        return getEnumByValue(clazz, value).isPresent();
+    }
+
+    static <E extends Enum<E> & ValueEnum<T>, T> Optional<E> getEnumByValue(@NonNull Class<E> clazz, T value) {
+        return value == null ? Optional.empty() : EnumUtils.getEnumList(clazz).stream()
+                .filter(c -> value.equals(c.getValue())).findFirst();
+    }
+
+    static <E extends Enum<E> & ValueEnum<T>, T> Optional<E> getEnumByOrdinal(@NonNull Class<E> clazz, Integer ordinal) {
+        return ordinal == null ? Optional.empty() : EnumUtils.getEnumList(clazz).stream()
+                .filter(p -> p.ordinal() == ordinal).findFirst();
+    }
+
+    static <E extends Enum<E> & ValueEnum<T>, T> T getValueByOrdinal(@NonNull Class<E> clazz, Integer ordinal) {
+        return ValueEnum.getEnumByOrdinal(clazz, ordinal).map(E::getValue).orElse(null);
+    }
+
+    static <E extends Enum<E> & ValueEnum<T>, T> Integer getOrdinalByValue(@NonNull Class<E> clazz, T value) {
+        return ValueEnum.getEnumByValue(clazz, value).map(Enum::ordinal).orElse(null);
+    }
+}
