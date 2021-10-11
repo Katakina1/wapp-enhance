84782684b605869ee55739a781ea4bf29e7fa8b4
马龙 <malong@xforceplus.com>
2021-10-11 19:36:50 +0800

    例外报告controller

--- wapp-core/src/main/java/com/xforceplus/wapp/config/SwaggerConfig.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/config/SwaggerConfig.java
@@ -24,8 +24,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.xforceplus.wapp.controller"))
-                .apis(RequestHandlerSelectors.basePackage("com.xforceplus.wapp.modules"))
+                .apis(RequestHandlerSelectors.basePackage("com.xforceplus.wapp"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- wapp-core/src/main/java/com/xforceplus/wapp/modules/exceptionreport/controller/ExceptionReportController.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/exceptionreport/controller/ExceptionReportController.java
@@ -1,5 +1,8 @@
 package com.xforceplus.wapp.modules.exceptionreport.controller;
 
+import com.xforceplus.wapp.annotation.EnhanceApiV1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2021-10-11 16:19
  **/
 @RestController
+@RequestMapping(EnhanceApiV1.BASE_PATH+"/exception-report")
 public class ExceptionReportController {
+
+    @GetMapping
+    public String get(){
+        return "hello";
+    }
 }
