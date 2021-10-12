80f4917a0faa7d58a11bd429045b139ec031da2a
汪名健 <wangmingjian@xforceplus.com>
2021-10-12 17:25:25 +0800

    init SFTPRemoter

---
+++ wapp-core/src/main/java/com/xforceplus/wapp/util/LocalFileSystemManager.java
@@ -0,0 +1,67 @@
+package com.xforceplus.wapp.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @program: wapp-enhance
+ * @description: manage local file system
+ * @author: Kenny Wong
+ * @create: 2021-10-12 16:41
+ **/
+public class LocalFileSystemManager {
+
+    private LocalFileSystemManager() {
+    }
+
+    /**
+     * 获取本地文件名列表
+     *
+     * @param path             文件路径
+     * @param fileNameKeyWords 文件名关键字
+     * @throws FileNotFoundException
+     */
+    public static List<String> getFileNames(String path, String fileNameKeyWords) throws FileNotFoundException {
+        File directory = new File(path);
+        // 文件夹是否存在
+        if (!directory.exists()) {
+            // 创建文件夹
+            if (!directory.mkdir()) {
+                throw new FileNotFoundException();
+            }
+
+            // 可写
+            if (!directory.canWrite() && !directory.setWritable(true)) {
+                throw new FileNotFoundException();
+            }
+        }
+        String[] fileNames = directory.list((dir, name) -> name.contains(fileNameKeyWords));
+        Optional<String[]> fileNamesOptional = Optional.ofNullable(fileNames);
+        return fileNamesOptional.map(Arrays::asList).orElseGet(Collections::emptyList);
+    }
+
+    /**
+     * 如果目录不存在，那么创建它
+     *
+     * @param directory 文件夹路径
+     * @throws FileNotFoundException
+     */
+    public static void createFolderIfNonExist(String directory) throws FileNotFoundException {
+        File localFolder = new File(directory);
+        // 文件夹是否存在
+        if (!localFolder.exists()) {
+            // 创建文件夹
+            if (!localFolder.mkdirs()) {
+                throw new FileNotFoundException();
+            }
+            // 可写
+            if (!localFolder.canWrite() && !localFolder.setWritable(true)) {
+                throw new FileNotFoundException();
+            }
+        }
+    }
+}
