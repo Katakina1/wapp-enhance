e862b437f0f2fd49783282f2980e9a15ea6e9ee2
lisongqiu <lisongqiu@xforceplus.com>
2021-10-12 14:22:36 +0800

    无效代码

--- wapp-domain/src/main/java/com/xforceplus/wapp/repository/dao/TXBillDeductDao.java
+++
@@ -1,8 +0,0 @@
-package com.xforceplus.wapp.repository.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xforceplus.wapp.repository.entity.TXBillDeductEntity;
-
-public interface TXBillDeductDao extends BaseMapper<TXBillDeductEntity> {
-
-}
