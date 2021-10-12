1e38e591b41b0ae69b9d149df04efa0c18580f58
马龙 <malong@xforceplus.com>
2021-10-12 17:21:40 +0800

    集成shiro和一期的账号体系

---
+++ wapp-core/src/main/java/com/xforceplus/wapp/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,67 @@
+package com.xforceplus.wapp.interceptor;
+
+
+import com.xforceplus.wapp.annotation.AuthIgnore;
+import com.xforceplus.wapp.common.exception.EnhanceRuntimeException;
+import com.xforceplus.wapp.modules.sys.entity.TokenEntity;
+import com.xforceplus.wapp.modules.sys.service.TokenService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 权限(Token)验证
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-23 15:38
+ */
+@Component
+public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
+    public static final String USER_KEY = "userId";
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        AuthIgnore annotation;
+        if (handler instanceof HandlerMethod) {
+            annotation = ((HandlerMethod) handler).getMethodAnnotation(AuthIgnore.class);
+        } else {
+            return true;
+        }
+
+        //如果有@IgnoreAuth注解，则不验证token
+        if (annotation != null) {
+            return true;
+        }
+
+        //从header中获取token
+        String token = request.getHeader("token");
+        //如果header中不存在token，则从参数中获取token
+        if (StringUtils.isBlank(token)) {
+            token = request.getParameter("token");
+        }
+
+        //token为空
+        if (StringUtils.isBlank(token)) {
+            throw new EnhanceRuntimeException("token不能为空");
+        }
+
+        //查询token信息
+        TokenEntity tokenEntity = tokenService.queryByToken(token);
+        if (tokenEntity == null || tokenEntity.getExpireTime().getTime() < System.currentTimeMillis()) {
+            throw new EnhanceRuntimeException("token失效，请重新登录");
+        }
+
+        //设置userId到request里，后续根据userId，获取用户信息
+        request.setAttribute(USER_KEY, tokenEntity.getUserId());
+
+        return true;
+    }
+}
---
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/sys/service/ShiroService.java
@@ -0,0 +1,38 @@
+package com.xforceplus.wapp.modules.sys.service;
+
+import com.xforceplus.wapp.modules.sys.entity.UserEntity;
+import com.xforceplus.wapp.modules.sys.entity.UserTokenEntity;
+
+/**
+ * shiro相关接口
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-06-06 8:49
+ */
+public interface ShiroService {
+
+    /**
+     * 查询UserTokenEntity
+     *
+     * @param schemaLabel
+     * @param token
+     * @return
+     */
+    UserTokenEntity queryByToken(String schemaLabel, String token);
+
+    /**
+     * 根据用户ID，查询用户
+     *
+     * @param userId
+     */
+    UserEntity queryUser(String schemaLabel, Long userId);
+
+    /**
+     * 更新UserTokenEntity
+     *
+     * @param schemaLabel
+     * @param token
+     */
+    int updateToken(String schemaLabel, UserTokenEntity token);
+}
