b986d9674cbf3dcb3651f6ec3bab5763b79dda0e
lisongqiu <lisongqiu@xforceplus.com>
2021-10-12 14:50:51 +0800

    申请索赔单不定案

--- wapp-core/src/main/java/com/xforceplus/wapp/enums/XfPreInvoiceStatusEnum.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/enums/XfPreInvoiceStatusEnum.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 /**
  * 预制发票状态
  */
-public enum XfPreInvoiceEnum {
+public enum XfPreInvoiceStatusEnum {
 
     NO_APPLY_RED_NOTIFICATION(1,"待申请红字信息"),
-    NO_UPLOAD_RED_INVOOCE(2,"待上传红票"),
-    UPLOAD_RED_INVOOCE(3,"已上传上传红票"),
+    NO_UPLOAD_RED_INVOICE(2,"待开红票"),
+    UPLOAD_RED_INVOICE(3,"已开红票"),
     WAIT_CHECK(4,"待审核"),
     CANCEL(5,"已撤销"),
     ;
@@ -19,7 +19,7 @@ public enum XfPreInvoiceEnum {
     @Getter
     private String desc;
 
-    XfPreInvoiceEnum(Integer code,String desc){
+    XfPreInvoiceStatusEnum(Integer code, String desc){
         this.code = code;
         this.desc = desc;
     }
--- wapp-core/src/main/java/com/xforceplus/wapp/modules/claim/service/ClaimService.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/claim/service/ClaimService.java
@@ -1,9 +1,18 @@
 package com.xforceplus.wapp.modules.claim.service;
 
-import com.xforceplus.wapp.enums.XfPreInvoiceEnum;
-import com.xforceplus.wapp.repository.dao.TXfPreInvoiceDao;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.xforceplus.wapp.common.exception.EnhanceRuntimeException;
+import com.xforceplus.wapp.enums.TXfBillDeductStatusEnum;
+import com.xforceplus.wapp.enums.XfPreInvoiceStatusEnum;
+import com.xforceplus.wapp.enums.XfSettlementStatusEnum;
+import com.xforceplus.wapp.repository.dao.*;
+import com.xforceplus.wapp.repository.entity.TXfBillDeductEntity;
+import com.xforceplus.wapp.repository.entity.TXfPreInvoiceEntity;
+import com.xforceplus.wapp.repository.entity.TXfSettlementEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -15,61 +24,106 @@ public class ClaimService {
 
     @Autowired
     private TXfPreInvoiceDao tXfPreInvoiceDao;
+    @Autowired
+    private TXfBillDeductDao tXfBillDeductDao;
+    @Autowired
+    private TXfBillDeductItemDao tXfBillDeductItemDao;
+    @Autowired
+    private TXfSettlementDao tXfSettlementDao;
+    @Autowired
+    private TXfSettlementItemDao tXfSettlementItemDao;
 
 
     /**
      * 申请索赔单不定案
-     * @param settlementId 结算单id
+     *
+     * @param settlementId     结算单id
      * @param billDeductIdList 索赔单id
      * @return
      */
-    public boolean applyClaimVerdict(Long settlementId, List<Long> billDeductIdList){
+    @Transactional
+    public boolean applyClaimVerdict(Long settlementId, List<Long> billDeductIdList) {
+        if (settlementId == null || CollectionUtils.isEmpty(billDeductIdList)) {
+            throw new EnhanceRuntimeException("参数异常");
+        }
+        //结算单
+        TXfSettlementEntity tXfSettlementEntity = tXfSettlementDao.selectById(settlementId);
+        if (tXfSettlementEntity == null) {
+            throw new EnhanceRuntimeException("结算单不存在");
+        }
+        //索赔单
+        List<TXfBillDeductEntity> billDeductList = tXfBillDeductDao.selectBatchIds(billDeductIdList);
+        //预制发票
+        QueryWrapper<TXfPreInvoiceEntity> wrapper = new QueryWrapper<>();
+        wrapper.eq(TXfPreInvoiceEntity.SETTLEMENT_NO, tXfSettlementEntity.getSettlementNo());
+        List<TXfPreInvoiceEntity> tXfPreInvoiceEntityList = tXfPreInvoiceDao.selectList(wrapper);
 
+        //修改预制发票状态
+        tXfPreInvoiceEntityList.forEach(tXfPreInvoiceEntity -> {
+            TXfPreInvoiceEntity updateTXfPreInvoiceEntity = new TXfPreInvoiceEntity();
+            updateTXfPreInvoiceEntity.setId(tXfPreInvoiceEntity.getId());
+            updateTXfPreInvoiceEntity.setPreInvoiceStatus(XfPreInvoiceStatusEnum.WAIT_CHECK.getCode());
+            tXfPreInvoiceDao.updateById(updateTXfPreInvoiceEntity);
+        });
 
+        //修改结算单状态
+        TXfSettlementEntity updateTXfSettlementEntity = new TXfSettlementEntity();
+        updateTXfSettlementEntity.setId(tXfSettlementEntity.getId());
+        updateTXfSettlementEntity.setSettlementStatus(XfSettlementStatusEnum.WAIT_CHECK.getCode());
 
+        //修改索赔单状态
+        billDeductList.forEach(tXfBillDeduct -> {
+            TXfBillDeductEntity updateTXfBillDeductEntity = new TXfBillDeductEntity();
+            updateTXfBillDeductEntity.setId(tXfBillDeduct.getId());
+            updateTXfBillDeductEntity.setStatus(TXfBillDeductStatusEnum.WAIT_CHECK.getCode());
+            tXfBillDeductDao.updateById(updateTXfBillDeductEntity);
+        });
         return true;
     }
 
     /**
      * 驳回申请索赔单不定案
-     * @param settlementId 结算单id
+     *
+     * @param settlementId     结算单id
      * @param billDeductIdList 索赔单id
      * @return
      */
-    public boolean rejectClaimVerdict(Long settlementId, List<Long> billDeductIdList){
+    public boolean rejectClaimVerdict(Long settlementId, List<Long> billDeductIdList) {
         return true;
     }
 
     /**
      * 通过申请索赔单不定案
-     * @param settlementId 结算单id
+     *
+     * @param settlementId     结算单id
      * @param billDeductIdList 索赔单id
      * @return
      */
-    public boolean agreeClaimVerdict(Long settlementId, List<Long> billDeductIdList){
+    public boolean agreeClaimVerdict(Long settlementId, List<Long> billDeductIdList) {
         return true;
     }
 
     /**
      * 作废结算单 结算单不能再次匹配 明细释放额度 蓝票释放额度
-     * @param settlementId 结算单id
+     *
+     * @param settlementId     结算单id
      * @param billDeductIdList 索赔单id
      * @return
      */
-    public boolean repealClaimSettlement(Long settlementId, List<Long> billDeductIdList){
+    public boolean repealClaimSettlement(Long settlementId, List<Long> billDeductIdList) {
         return true;
     }
 
     /**
      * 撤销结算单 结算单能再次匹配 明细释放额度 蓝票释放额度
-     * @param settlementId 结算单id
+     *
+     * @param settlementId     结算单id
      * @param billDeductIdList 索赔单id
      * @return
      */
-    public boolean backOutClaimSettlement(Long settlementId, List<Long> billDeductIdList){
+    public boolean backOutClaimSettlement(Long settlementId, List<Long> billDeductIdList) {
         return true;
     }
-
 
 
 }
