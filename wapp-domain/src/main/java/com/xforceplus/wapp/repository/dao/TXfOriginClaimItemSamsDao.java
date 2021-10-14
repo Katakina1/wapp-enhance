47a145a26be26e6bafb008141c61b78fbfaaf9a0
汪名健 <wangmingjian@xforceplus.com>
2021-10-14 10:59:30 +0800

    fix compiler error

--- wapp-domain/src/main/java/com/xforceplus/wapp/repository/dao/TXfOriginClaimItemSamsDao.java
+++ wapp-domain/src/main/java/com/xforceplus/wapp/repository/dao/TXfOriginClaimItemSamsDao.java
@@ -10,7 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * </p>
 *
 * @author malong@xforceplus.com
-* @since 2021-10-13
+* @since 2021-10-14
 */
 public interface TXfOriginClaimItemSamsDao extends BaseMapper<TXfOriginClaimItemSamsEntity> {
 
