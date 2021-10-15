84bc56f0d4c7e4f9e26b45b79599f2fc36c21317
马龙 <malong@xforceplus.com>
2021-10-15 16:00:50 +0800

    bigdecimal 类型转换

--- wapp-generator/src/main/java/com/xforceplus/generator/CodeGenerator.java
+++ wapp-generator/src/main/java/com/xforceplus/generator/CodeGenerator.java
@@ -6,8 +6,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.converts.SqlServerTypeConvert;
 import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.IColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
@@ -70,26 +72,15 @@ public class CodeGenerator {
         dsc.setUsername("testuser01");
         dsc.setPassword("aGSsdsJFWd_w_1");
         dsc.setDriverName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//        dsc.setTypeConvert(new MySqlTypeConvert() {
-//            @Override
-//            public IColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
-//                if (fieldType.toLowerCase().contains("json")) {
-//                    return new IColumnType() {
-//                        @Override
-//                        public String getType() {
-////                              泛型栗子  return "Map<String,Object>";
-//                            return "BillExtendMap";
-//                        }
-//
-//                        @Override
-//                        public String getPkg() {
-//                            return "com.xforceplus.receipt.typehandler.BillExtendMap";
-//                        }
-//                    };
-//                }
-//                return super.processTypeConvert(globalConfig, fieldType);
-//            }
-//        });
+        dsc.setTypeConvert(new SqlServerTypeConvert() {
+            @Override
+            public IColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
+                if ( fieldType.toLowerCase().contains( "decimal" ) ) {
+                    return DbColumnType.BIG_DECIMAL;
+                }
+                return super.processTypeConvert(globalConfig, fieldType);
+            }
+        });
 
         mpg.setDataSource(dsc);
 
