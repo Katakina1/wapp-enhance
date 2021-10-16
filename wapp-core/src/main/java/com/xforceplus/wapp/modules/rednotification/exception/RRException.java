e82c4407038901a04c418017f0b41732c7b8a21b
马龙 <malong@xforceplus.com>
2021-10-16 20:21:32 +0800

    打开身份验真

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/rednotification/exception/RRException.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/rednotification/exception/RRException.java
@@ -1,5 +1,7 @@
 package com.xforceplus.wapp.modules.rednotification.exception;
 
+import com.xforceplus.wapp.common.exception.EnhanceRuntimeException;
+
 /**
  * 自定义异常
  *
@@ -7,49 +9,26 @@ package com.xforceplus.wapp.modules.rednotification.exception;
  * @email sunlightcs@gmail.com
  * @date 2016年10月27日 下午10:11:27
  */
-public class RRException extends RuntimeException {
+public class RRException extends EnhanceRuntimeException {
     private static final long serialVersionUID = 1L;
 
-    private String msg;
-    private int code = 500;
 
     public RRException(String msg) {
         super(msg);
-        this.msg = msg;
     }
 
     public RRException(String msg, Throwable e) {
         super(msg, e);
-        this.msg = msg;
     }
 
-    public RRException(String msg, int code) {
-        super(msg);
-        this.msg = msg;
-        this.code = code;
+    public RRException(String msg, String code) {
+        super(msg,code);
     }
 
-    public RRException(String msg, int code, Throwable e) {
-        super(msg, e);
-        this.msg = msg;
-        this.code = code;
+    public RRException(String msg, String code, Throwable e) {
+        super(msg, e,code);
     }
 
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
 
 
 }
