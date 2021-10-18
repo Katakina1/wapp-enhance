d490fe60965319f551f84a6080cb4127d7c7e08a
lisongqiu <lisongqiu@xforceplus.com>
2021-10-18 14:30:14 +0800

    申请不定案

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/settlement/controller/SettlementController.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/settlement/controller/SettlementController.java
@@ -1,6 +1,15 @@
 package com.xforceplus.wapp.modules.settlement.controller;
 
 import com.xforceplus.wapp.annotation.EnhanceApi;
+import com.xforceplus.wapp.modules.claim.dto.ApplyVerdictRequest;
+import com.xforceplus.wapp.modules.claim.service.ClaimService;
+import com.xforceplus.wapp.modules.rednotification.model.Response;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(EnhanceApi.BASE_PATH + "/settlement")
 public class SettlementController {
 
+    @Autowired
+    private ClaimService claimService;
+
+    @ApiOperation(value = "申请不定案", notes = "", response = Response.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "response", response = Response.class)})
+    @PostMapping(value = "/applyVerdict")
+    public Response applyVerdict(@RequestBody ApplyVerdictRequest request) {
+        claimService.applyClaimVerdict(request.getSettlementId(), request.getBillDeductIdList());
+        return Response.ok("成功", "");
+    }
 
 }
