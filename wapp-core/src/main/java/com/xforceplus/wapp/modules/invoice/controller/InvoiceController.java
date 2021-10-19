8738f739aa3394add9bd091b3508d661deccd202
杨申 <yangshen@xforceplus.com>
2021-10-19 17:32:54 +0800

    发票池详情接口

---
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/invoice/controller/InvoiceController.java
@@ -0,0 +1,29 @@
+package com.xforceplus.wapp.modules.invoice.controller;
+
+import com.xforceplus.wapp.modules.invoice.dto.InvoiceDto;
+import com.xforceplus.wapp.modules.invoice.service.InvoiceService;
+import com.xforceplus.wapp.modules.rednotification.model.Response;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/invoice")
+public class InvoiceController {
+
+    @Autowired
+    InvoiceService invoiceService;
+
+    @ApiOperation(value = "发票详情", notes = "", response = Response.class, tags = {"发票池",})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "response", response = Response.class)})
+    @GetMapping(value = "/detail")
+    public Response<InvoiceDto> detail(@RequestParam(value="id") Long id){
+        return invoiceService.detail(id);
+    }
+}
