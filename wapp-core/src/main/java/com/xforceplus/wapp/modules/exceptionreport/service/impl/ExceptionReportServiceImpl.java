0a876a189e6e114d558dc71b33a74e6967219e5d
马龙 <malong@xforceplus.com>
2021-10-19 13:50:12 +0800

    导出队列

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/exceptionreport/service/impl/ExceptionReportServiceImpl.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/exceptionreport/service/impl/ExceptionReportServiceImpl.java
@@ -1,21 +1,26 @@
 package com.xforceplus.wapp.modules.exceptionreport.service.impl;
 
+import cn.hutool.poi.excel.BigExcelWriter;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xforceplus.wapp.common.utils.DateUtils;
 import com.xforceplus.wapp.enums.exceptionreport.ExceptionReportTypeEnum;
+import com.xforceplus.wapp.modules.backFill.service.FileService;
 import com.xforceplus.wapp.modules.exceptionreport.dto.ExceptionReportRequest;
 import com.xforceplus.wapp.modules.exceptionreport.mapstruct.ExceptionReportMapper;
 import com.xforceplus.wapp.modules.exceptionreport.service.ExceptionReportService;
 import com.xforceplus.wapp.repository.dao.TXfExceptionReportDao;
 import com.xforceplus.wapp.repository.entity.TXfExceptionReportEntity;
 import com.xforceplus.wapp.sequence.IDSequence;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
@@ -24,6 +29,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author malong@xforceplus.com
@@ -42,6 +48,9 @@ public class ExceptionReportServiceImpl extends ServiceImpl<TXfExceptionReportDa
 
     @Autowired
     private ExceptionReportMapper exceptionReportMapper;
+
+    @Autowired
+    private FileService fileService;
 
 
     @Override
@@ -97,14 +106,32 @@ public class ExceptionReportServiceImpl extends ServiceImpl<TXfExceptionReportDa
     }
     @Override
     public void export(ExceptionReportRequest request, ExceptionReportTypeEnum typeEnum) {
-        Page<TXfExceptionReportEntity> page = new Page<>();
-        page.setSize(request.getSize());
-        page.setCurrent(request.getPage());
+        List<TXfExceptionReportEntity> list = getExportData(request,typeEnum,0);
+        try (BigExcelWriter bigExcelWriter = new BigExcelWriter()) {
+            while (CollectionUtils.isNotEmpty(list)) {
+                bigExcelWriter.write(list);
+                long lastId = list.get(list.size() - 1).getId();
+                list = getExportData(request, typeEnum, lastId);
+            }
+            // TODO
+            ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+            bigExcelWriter.flush(outputStream);
+            try {
+                final String file = fileService.uploadFile(outputStream.toByteArray(), "");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private List<TXfExceptionReportEntity> getExportData(ExceptionReportRequest request, ExceptionReportTypeEnum typeEnum,long lastId){
         TXfExceptionReportEntity entity = exceptionReportMapper.toEntity(request);
         entity.setType(typeEnum.getType());
         final String startDeductDate = request.getStartDeductDate();
         final LambdaQueryWrapper<TXfExceptionReportEntity> wrapper = Wrappers.lambdaQuery(entity);
 
+        wrapper.gt(TXfExceptionReportEntity::getId,lastId);
         if (StringUtils.isNotBlank(startDeductDate)) {
             wrapper.gt(TXfExceptionReportEntity::getCreateTime, startDeductDate);
         }
@@ -118,6 +145,9 @@ public class ExceptionReportServiceImpl extends ServiceImpl<TXfExceptionReportDa
                 log.error("时间转换失败" + e.getMessage(), e);
             }
         }
+        wrapper.orderByAsc(TXfExceptionReportEntity::getId);
+
+        return this.list(wrapper);
 
     }
 }
