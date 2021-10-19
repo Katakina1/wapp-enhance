64f78c948beb59a98539e339b1ae7c8baaaba00a
zhangzhenwei@xforeplus.com <Ab123456>
2021-10-19 09:41:56 +0800

    预制发票拆票

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/preinvoice/service/PreinvoiceService.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/preinvoice/service/PreinvoiceService.java
@@ -1,5 +1,8 @@
 package com.xforceplus.wapp.modules.preinvoice.service;
-import com.xforceplus.phoenix.split.model.SplitPreInvoiceInfo;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
+import com.xforceplus.phoenix.split.model.*;
+import com.xforceplus.wapp.common.utils.BeanUtil;
 import com.xforceplus.wapp.dto.SplitRuleInfoDTO;
 import com.xforceplus.wapp.enums.TXfSettlementStatusEnum;
 import com.xforceplus.wapp.modules.company.service.CompanyService;
@@ -13,9 +16,14 @@ import com.xforceplus.wapp.service.CommRedNotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 类描述：
@@ -28,7 +36,35 @@ import java.util.List;
 @Service
 @Slf4j
 public class PreinvoiceService {
-    @Autowired
+    static final String RULE_INFO = " {\n" +
+            "\t\t\"amountSplitRule\": \"2\",\n" +
+            "\t\t\"cargoNameLength\": 92,\n" +
+            "\t\t\"customRemarkSize\": 200,\n" +
+            "\t\t\"ignoreAllowableError\": false,\n" +
+            "\t\t\"invoiceItemMaxRow\": 7,\n" +
+            "\t\t\"invoiceLimit\": 999999.99,\n" +
+            "\t\t\"itemSort\": \"1\",\n" +
+            "\t\t\"itemSpecNameLength\": 36,\n" +
+            "\t\t\"limitIsAmountWithTax\": false,\n" +
+            "\t\t\"mergeBySplitFiled\": false,\n" +
+            "\t\t\"priceMethod\": \"WITHOUT_TAX\",\n" +
+            "\t\t\"remarkDuplicateFlag\": true,\n" +
+            "\t\t\"remarkFiledMetadataBeanList\": [{\n" +
+            "\t\t\t\"fieldDisplayName\": \"备注\",\n" +
+            "\t\t\t\"fieldGroupIndex\": \"BILL_INFO\",\n" +
+            "\t\t\t\"fieldName\": \"remark\"\n" +
+            "\t\t}],\n" +
+            "\t\t\"ruleId\": 687162585176363000,\n" +
+            "\t\t\"saleListOption\": \"1\",\n" +
+            "\t\t\"salesListMaxRow\": 2000,\n" +
+            "\t\t\"showSpecification\": true,\n" +
+            "\t\t\"splitByItemPriceQuantityNon\": false,\n" +
+            "\t\t\"splitFiledList\": [\"taxRate\"],\n" +
+            "\t\t\"unitPriceAmountOps\": \"0\",\n" +
+            "\t\t\"unitPriceScale\": 4,\n" +
+            "\t\t\"zeroTaxOption\": \"NOT_PROCESS\"\n" +
+            "\t}";
+     @Autowired
     private CommRedNotificationService commRedNotificationService;
     @Autowired
     private TXfSettlementExtDao tXfSettlementDao;
@@ -36,6 +72,10 @@ public class PreinvoiceService {
     private TXfSettlementItemExtDao tXfSettlementItemDao;
     @Autowired
     CompanyService companyService;
+    @Autowired
+    private RestTemplate restTemplate;
+    // 配置访问域名
+    private String domainUrl;
     /**
      * 拆票方法
      * @param settlementNo
@@ -46,6 +86,13 @@ public class PreinvoiceService {
         TXfSettlementEntity tXfSettlementEntity =  tXfSettlementDao.querySettlementByNo( settlementNo,TXfSettlementStatusEnum.WAIT_SPLIT_INVOICE.getCode() );
         List<TXfSettlementItemEntity> tXfSettlementItemEntities = tXfSettlementItemDao.queryItemBySettlementNo(settlementNo);
         TAcOrgEntity tAcOrgEntity = companyService.getOrgInfoByOrgCode(sellerNo, "8");
+        CreatePreInvoiceParam createPreInvoiceParam = assembleParam(tXfSettlementEntity, tXfSettlementItemEntities, tAcOrgEntity);
+        Map<String, Object> param = Maps.newHashMap();
+        param.put("tenantId", 0);
+        param.put("appId", 3);
+        param.put("returnMode", "sync");
+        param.put("taxDeviceType", 3);
+        ResponseEntity<BaseResponse> res = restTemplate.postForEntity(domainUrl, createPreInvoiceParam, BaseResponse.class, param);
         //查询待拆票算单主信息，
         //查询结算单明细信息
         //查询开票信息
@@ -56,6 +103,41 @@ public class PreinvoiceService {
         return null;
     }
 
+    /**
+     * 拼接拆票请求
+     * @param tXfSettlementEntity
+     * @param tXfSettlementItemEntities
+     * @param tAcOrgEntity
+     * @return
+     */
+    private CreatePreInvoiceParam assembleParam(TXfSettlementEntity tXfSettlementEntity,List<TXfSettlementItemEntity> tXfSettlementItemEntities,TAcOrgEntity tAcOrgEntity) {
+        CreatePreInvoiceParam createPreInvoiceParam = new CreatePreInvoiceParam();
+        BillInfo billInfo = new BillInfo();
+        BeanUtil.copyProperties(tXfSettlementEntity, billInfo);
+        billInfo.setPriceMethod(1);
+        billInfo.setPurchaserId(0L);
+        billInfo.setPurchaserGroupId(0L);
+        billInfo.setPurchaserTenantId(0l);
+        billInfo.setSellerId(0L);
+        billInfo.setSellerTenantId(0l);
+        billInfo.setSellerGroupId(0l);
+        List<BillItem> billItems = new ArrayList<>();
+        BeanUtil.copyList( tXfSettlementItemEntities,billItems,BillItem.class);
+        String settlementNo = tXfSettlementEntity.getSettlementNo();
+        int i = 0;
+        for (BillItem billItem : billItems) {
+            billItem.setSalesbillId(settlementNo );
+            billItem.setSalesbillItemId(settlementNo + i);
+            billItem.setSalesbillNo(settlementNo);
+            billItem.setSalesbillItemNo(settlementNo + i);
+        }
+        billInfo.setBillItems(billItems);
+        SplitRule splitRule =    JSONObject.parseObject(RULE_INFO, SplitRule.class);
+        splitRule.setInvoiceLimit(BigDecimal.valueOf(tAcOrgEntity.getQuota()));
+        createPreInvoiceParam.setBillInfo(billInfo);
+        createPreInvoiceParam.setRule(splitRule);
+        return createPreInvoiceParam;
+    }
     /**
      * 重新拆票
      * @param settlementNo
