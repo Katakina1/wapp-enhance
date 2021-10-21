9b2abc5679318b5d72057a4749fe9a3a616275e5
zhangzhenwei@xforeplus.com <Ab123456>
2021-10-21 13:46:14 +0800

    定时器拆分

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/deduct/schedule/AgreementDeductScheduler.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/deduct/schedule/AgreementDeductScheduler.java
@@ -1,5 +1,8 @@
 package com.xforceplus.wapp.modules.deduct.schedule;
-import com.xforceplus.wapp.modules.deduct.service.DeductService;
+import com.xforceplus.wapp.enums.TXfBillDeductStatusEnum;
+import com.xforceplus.wapp.enums.XFDeductionBusinessTypeEnum;
+import com.xforceplus.wapp.modules.deduct.service.AgreementService;
+import com.xforceplus.wapp.repository.entity.TXfBillDeductEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,9 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class AgreementDeductScheduler {
     @Autowired
-    private DeductService deductService;
+    private AgreementService agreementService;
     @Scheduled(cron=" 0 0 0 */7 * ?") //每七天执行一次
     public void AgreementDeductDeal(){
-       // deductService.receiveDone(XFDeductionBusinessTypeEnum.AGREEMENT_BILL);
-    }
+
+        agreementService.mergeEPDandAgreementSettlement(XFDeductionBusinessTypeEnum.AGREEMENT_BILL, TXfBillDeductStatusEnum.AGREEMENT_NO_MATCH_SETTLEMENT, TXfBillDeductStatusEnum.AGREEMENT_NO_MATCH_SETTLEMENT, null);
+     }
 }
