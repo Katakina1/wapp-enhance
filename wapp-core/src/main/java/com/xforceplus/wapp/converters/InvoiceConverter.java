a7b03718a8fe349cdde1f1c31a18da4c8204906c
马少鹏 <mashaopeng@xforceplus.com>
2021-10-23 11:10:02 +0800

    修改代码

--- wapp-core/src/main/java/com/xforceplus/wapp/converters/InvoiceConverter.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/converters/InvoiceConverter.java
@@ -4,6 +4,7 @@ import com.xforceplus.wapp.handle.vo.InvoiceVo;
 import com.xforceplus.wapp.repository.entity.TXfInvoiceEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 /**
  * @author mashaopeng@xforceplus.com
@@ -12,4 +13,9 @@ import org.mapstruct.Mapping;
 public interface InvoiceConverter {
     @Mapping(target = "paperDrewDate", source = "paperDrewDate", dateFormat = "yyyyMMdd")
     TXfInvoiceEntity map(InvoiceVo.Invoice data);
+
+    @Named("mapStatus")
+    default String mapStatus(String status) {
+        return null;
+    }
 }
--- wapp-core/src/main/java/com/xforceplus/wapp/modules/overdue/service/OverdueServiceImpl.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/overdue/service/OverdueServiceImpl.java
@@ -59,20 +59,22 @@ public class OverdueServiceImpl extends ServiceImpl<OverdueDao, OverdueEntity> {
         return Tuple.of(overdueConverter.map(page.getRecords()), page);
     }
 
-    public Optional<Overdue> oneOptBySellerNo(@NonNull ServiceTypeEnum typeEnum, @NonNull String sellerNo) {
+    public Overdue oneOptBySellerNo(@NonNull ServiceTypeEnum typeEnum, @NonNull String sellerNo) {
         log.info("超期配置查询,入参：{}", sellerNo);
         return new LambdaQueryChainWrapper<>(getBaseMapper())
                 .isNull(OverdueEntity::getDeleteFlag)
                 .eq(OverdueEntity::getType, typeEnum.getValue())
-                .eq(OverdueEntity::getSellerNo, sellerNo).oneOpt().map(overdueConverter::map);
+                .eq(OverdueEntity::getSellerNo, sellerNo).oneOpt()
+                .map(overdueConverter::map).orElseGet(null);
     }
 
-    public Optional<Overdue> oneOptBySellerTaxNo(@NonNull ServiceTypeEnum typeEnum, @NonNull String sellerTaxNo) {
+    public Overdue oneOptBySellerTaxNo(@NonNull ServiceTypeEnum typeEnum, @NonNull String sellerTaxNo) {
         log.info("超期配置查询,入参：{}", sellerTaxNo);
         return new LambdaQueryChainWrapper<>(getBaseMapper())
                 .isNull(OverdueEntity::getDeleteFlag)
                 .eq(OverdueEntity::getType, typeEnum.getValue())
-                .eq(OverdueEntity::getSellerTaxNo, sellerTaxNo).oneOpt().map(overdueConverter::map);
+                .eq(OverdueEntity::getSellerTaxNo, sellerTaxNo)
+                .oneOpt().map(overdueConverter::map).orElseGet(null);
     }
 
     public Either<String, Integer> export(ServiceTypeEnum typeEnum, InputStream is) {
