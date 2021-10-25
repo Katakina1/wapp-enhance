6cb56522c45c1fbfe3cfca385291ccba2ba4de48
杨申 <yangshen@xforceplus.com>
2021-10-25 14:44:04 +0800

    。。

---
+++ wapp-domain/src/main/java/com/xforceplus/wapp/repository/dao/TDxInvoiceDetailsDao.java
@@ -0,0 +1,16 @@
+package com.xforceplus.wapp.repository.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+* <p>
+* 发票明细表 Mapper 接口
+* </p>
+*
+* @author malong@xforceplus.com
+* @since 2021-10-25
+*/
+public interface TDxInvoiceDetailsDao extends BaseMapper<TDxInvoiceDetailsEntity> {
+
+}
