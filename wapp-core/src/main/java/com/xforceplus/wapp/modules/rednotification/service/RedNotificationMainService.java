e50547e135190c5e78d4db3028d039d8f4a5bef3
杨申 <yangshen@xforceplus.com>
2021-10-25 18:01:52 +0800

    回填失败原因

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/rednotification/service/RedNotificationMainService.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/rednotification/service/RedNotificationMainService.java
@@ -127,6 +127,13 @@ public class RedNotificationMainService extends ServiceImpl<TXfRedNotificationDa
                 queryModel.setPidList(pidList);
                 applyRequest.setQueryModel(queryModel);
             }else {
+                // 终端不在线更新到红字信息表
+                List<Long> redIdList = listMain.stream().map(item -> item.getId()).collect(Collectors.toList());
+                TXfRedNotificationEntity record = new TXfRedNotificationEntity();
+                record.setApplyRemark("税盘不在线");
+                LambdaUpdateWrapper<TXfRedNotificationEntity> updateWrapper = new LambdaUpdateWrapper<>();
+                updateWrapper.in(TXfRedNotificationEntity::getId,redIdList);
+                getBaseMapper().update(record,updateWrapper);
                 throw new RRException(String.format("未获取税号[%s]的在线终端",rednotificationMain.getPurchaserTaxNo()));
             }
             //申请
@@ -151,6 +158,14 @@ public class RedNotificationMainService extends ServiceImpl<TXfRedNotificationDa
             }else {
                 //更新流水.全部失败
                 updateRequestFail(applyRequest.getSerialNo(), taxWareResponse);
+                //更新失败原因到主表
+                List<Long> redIdList = redInfoList.stream().map(item -> Long.parseLong(item.getPid())).collect(Collectors.toList());
+                TXfRedNotificationEntity record = new TXfRedNotificationEntity();
+                record.setApplyRemark(taxWareResponse.getMessage());
+                LambdaUpdateWrapper<TXfRedNotificationEntity> updateWrapper = new LambdaUpdateWrapper<>();
+                updateWrapper.in(TXfRedNotificationEntity::getId,redIdList);
+                getBaseMapper().update(record,updateWrapper);
+
                 return  Response.failed(taxWareResponse.getMessage());
             }
         }else {
