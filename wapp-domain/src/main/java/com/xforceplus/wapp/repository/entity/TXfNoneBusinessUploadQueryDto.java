78c1cb0900e7ba5beefc4992d65be0a7dd276b3b
AIWENTAO <aiwentao@xforceplus.com>
2021-10-26 21:38:11 +0800

    非商上传接口改成批量

--- wapp-domain/src/main/java/com/xforceplus/wapp/repository/entity/TXfNoneBusinessUploadQueryDto.java
+++ wapp-domain/src/main/java/com/xforceplus/wapp/repository/entity/TXfNoneBusinessUploadQueryDto.java
@@ -61,4 +61,7 @@ public class TXfNoneBusinessUploadQueryDto {
     @ApiModelProperty("供应商编号")
     private String venderId;
 
+    @ApiModelProperty("是否提交 0未提交 1 已提交")
+    private String submitFlag;
+
 }
