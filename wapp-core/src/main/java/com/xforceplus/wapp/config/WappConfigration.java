9b4e69006b2c1c476bf25e9b147d6aa10c140a64
马龙 <malong@xforceplus.com>
2021-10-27 20:39:00 +0800

    api参数全局空字符串转null

--- wapp-core/src/main/java/com/xforceplus/wapp/config/WappConfigration.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/config/WappConfigration.java
@@ -2,21 +2,16 @@ package com.xforceplus.wapp.config;
 
 import com.xforceplus.wapp.sequence.IDSequence;
 import org.springframework.beans.PropertyEditorRegistrar;
-import org.springframework.beans.PropertyEditorRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
-import org.springframework.beans.support.ResourceEditorRegistrar;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.validation.DataBinder;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
-import org.springframework.web.bind.support.WebRequestDataBinder;
 import org.springframework.web.client.RestTemplate;
 
-import java.beans.PropertyEditor;
 import java.util.List;
 
 /**
@@ -46,9 +41,14 @@ public class WappConfigration {
     }
 
     @Bean
-    ConfigurableWebBindingInitializer configurableWebBindingInitializer(List<PropertyEditorRegistrar> propertyEditorRegistrars){
+    ConfigurableWebBindingInitializer configurableWebBindingInitializer(List<PropertyEditorRegistrar> propertyEditorRegistrars
+            , FormattingConversionService service
+            , Validator validator
+                                                                        ){
         final ConfigurableWebBindingInitializer initializer = new ConfigurableWebBindingInitializer();
         initializer.setPropertyEditorRegistrars(propertyEditorRegistrars.toArray(new PropertyEditorRegistrar[0]));
+        initializer.setConversionService(service);
+        initializer.setValidator(validator);
         return initializer;
     }
 
