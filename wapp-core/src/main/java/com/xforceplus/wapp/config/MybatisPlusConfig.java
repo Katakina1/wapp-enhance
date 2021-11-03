77accc3f1f8ea2322f1a399d94572e7bf097b725
杨申 <yangshen@xforceplus.com>
2021-11-03 15:02:54 +0800

    添加mybatis 插件使用行锁

--- wapp-core/src/main/java/com/xforceplus/wapp/config/MybatisPlusConfig.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/config/MybatisPlusConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Properties;
+
 @EnableTransactionManagement
 @Configuration
 @MapperScan({"com.xforceplus.wapp.repository","com.xforceplus.wapp.modules.sys.dao"})
@@ -20,4 +22,20 @@ public class MybatisPlusConfig {
 //    public SqlServerOperationInterceptor sqlServerOperationInterceptor(){
 //        return new SqlServerOperationInterceptor();
 //    }
+
+    /**
+     * 新增修改使用行锁
+     * @return
+     */
+    @Bean
+    public MybatisRowLockPlugin mybatisRowLockPlugin() {
+        MybatisRowLockPlugin mybatisRowLockPlugin = new MybatisRowLockPlugin();
+        //设置参数，比如阈值等，可以在配置文件中配置，这里直接写死便于测试
+        Properties properties = new Properties();
+        //这里设置慢查询阈值为1毫秒，便于测试
+        properties.setProperty("time", "1");
+        mybatisRowLockPlugin.setProperties(properties);
+        return mybatisRowLockPlugin;
+    }
+
 }
