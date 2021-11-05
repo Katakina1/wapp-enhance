ccad00b6aace8bf68f32ef1d3428c1b0b39e1aa0
zhangzhenwei@xforeplus.com <Ab123456>
2021-11-05 16:58:53 +0800

    税率问题

--- wapp-core/src/main/java/com/xforceplus/wapp/config/TaxRateConfig.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/config/TaxRateConfig.java
@@ -1,7 +1,6 @@
 package com.xforceplus.wapp.config;
 
 import com.google.common.collect.Maps;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -27,9 +26,27 @@ public class TaxRateConfig {
     @Value("${taxRateSet:''}")
     private String taxRateStr;
     private Map<BigDecimal, BigDecimal> taxRateMap = Maps.newHashMap();
-
+    private List<BigDecimal > res  ;
     public BigDecimal getNextTaxRate(BigDecimal taxRate) {
         return taxRateMap.get(BigDecimal.valueOf(taxRate.doubleValue()));
+    }
+
+    public Map<BigDecimal,BigDecimal> bulidTaxRateMap(BigDecimal taxRate) {
+        Map<BigDecimal, BigDecimal> tmp = new HashMap<>();
+        for (int i = 0; i < res.size(); i++) {
+            if (i == 0) {
+                tmp.put(taxRate, res.get(i));
+            }
+            if (i == res.size() - 1) {
+                tmp.put(res.get(i), null);
+                break;
+            }
+            if (taxRate.compareTo(res.get(i)) == 0) {
+                continue;
+            }
+            tmp.put(res.get(i), res.get(i + 1));
+        }
+        return tmp;
     }
 
     @PostConstruct
@@ -37,13 +54,13 @@ public class TaxRateConfig {
         if (StringUtils.isEmpty(taxRateStr)) {
             taxRateStr = "0.13,0.11,0.09,0.06,0.05,0.03,0.01,0.00";
         }
-        List<BigDecimal> res = Arrays.asList(taxRateStr.split(",")).stream().map(x -> new BigDecimal(x)).unordered().collect(Collectors.toList());
-        for (int i = 0; i < res.size(); i++) {
-            if (i == res.size() - 1) {
-                taxRateMap.put(res.get(i), null);
-                break;
-            }
-            taxRateMap.put(res.get(i), res.get(i + 1));
-        }
+        res = Arrays.asList(taxRateStr.split(",")).stream().map(x -> new BigDecimal(x)).unordered().collect(Collectors.toList());
     }
+//
+//    public static void main(String[] ags) {
+//        TaxRateConfig taxRateConfig = new TaxRateConfig();
+//        taxRateConfig.init();
+//        Map<BigDecimal, BigDecimal> tmp = taxRateConfig.bulidTaxRateMap(BigDecimal.valueOf(0.06));
+//        System.out.println(tmp);
+//    }
 }
