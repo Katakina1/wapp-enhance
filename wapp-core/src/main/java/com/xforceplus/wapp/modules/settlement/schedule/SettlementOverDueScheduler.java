226b061d564202db7b9cf7d2be0e732b28753795
zhangzhenwei@xforeplus.com <Ab123456>
2021-11-05 15:05:46 +0800

    结算单确认金额问题

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/settlement/schedule/SettlementOverDueScheduler.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/settlement/schedule/SettlementOverDueScheduler.java
@@ -1,5 +1,6 @@
 package com.xforceplus.wapp.modules.settlement.schedule;
 
+import com.xforceplus.wapp.common.utils.DateUtils;
 import com.xforceplus.wapp.enums.TXfSettlementStatusEnum;
 import com.xforceplus.wapp.modules.preinvoice.service.PreinvoiceService;
 import com.xforceplus.wapp.modules.settlement.service.SettlementService;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -28,6 +30,7 @@ public class SettlementOverDueScheduler {
     @Autowired
     private StringRedisTemplate redisTemplate;
     public static String KEY = "Settlement-overdraft";
+    private static Integer STEP = 5;
     /**
      * 自动确认逾期金额 结算单下 所属的单据 全部预期，自动确认
      */
@@ -44,8 +47,13 @@ public class SettlementOverDueScheduler {
             Integer status = TXfSettlementStatusEnum.WAIT_CONFIRM.getCode();
             Integer limit = 100;
             List<TXfSettlementEntity> list = settlementService.querySettlementByStatus(id, status, limit);
+            Date date = new Date();
+            Date reference = DateUtils.addDate(date, 0 - STEP);
             while (CollectionUtils.isNotEmpty(list)) {
                 for (TXfSettlementEntity tXfSettlementEntity : list) {
+                    if (tXfSettlementEntity.getCreateTime().before(reference)) {
+                        continue;
+                    }
                     try {
                         preinvoiceService.reCalculation( tXfSettlementEntity.getSettlementNo() );
                     } catch (Exception e) {
