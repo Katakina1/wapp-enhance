3e3d5cfaea7850260ebc74988867a4cbdcb8d5f3
杨申 <yangshen@xforceplus.com>
2021-11-09 20:27:10 +0800

    红字信息销货清单生成pdf

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/rednotification/service/RedNotificationMainService.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/rednotification/service/RedNotificationMainService.java
@@ -93,6 +93,8 @@ public class RedNotificationMainService extends ServiceImpl<TXfRedNotificationDa
     @Value("${wapp.rednotification.maxApply}")
     private Integer maxApply;
 
+    public static final int MAX_DETAIL_SIZE = 8;
+
 
 
     public Response add(AddRedNotificationRequest request) {
@@ -877,7 +879,13 @@ public class RedNotificationMainService extends ServiceImpl<TXfRedNotificationDa
             return detailInfo;
         }).collect(Collectors.toList());
 
-        redInfo.setDetails(detailInfos);
+        if(applyDetails.size() > MAX_DETAIL_SIZE){
+            RedGeneratePdfDetailInfo merge = merge(applyDetails);
+            redInfo.setDetails(Lists.newArrayList(merge));
+        }else {
+            redInfo.setDetails(detailInfos);
+        }
+
 
         redInfo.setOriginInvoiceCode(apply.getOriginInvoiceCode());
         redInfo.setOriginInvoiceNo(apply.getOriginInvoiceNo());
@@ -1091,6 +1099,39 @@ public class RedNotificationMainService extends ServiceImpl<TXfRedNotificationDa
     }
 
 
+
+    private RedGeneratePdfDetailInfo merge(List<TXfRedNotificationDetailEntity>  treatedRedNoDetails){
+        BigDecimal sumAmountWithoutTax = BigDecimal.ZERO;
+        BigDecimal sumTaxAmount = BigDecimal.ZERO;
+
+        boolean isMixedRate = false;
+        BigDecimal taxRate = null;
+
+        for(TXfRedNotificationDetailEntity redNoDetailInfo : treatedRedNoDetails){
+            sumAmountWithoutTax = sumAmountWithoutTax.add(redNoDetailInfo.getAmountWithoutTax());
+            sumTaxAmount = sumTaxAmount.add(redNoDetailInfo.getTaxAmount());
+            if(Objects.isNull(taxRate)){
+                taxRate = redNoDetailInfo.getTaxRate();
+            }else{
+                if(!isMixedRate){
+                    if(!taxRate.equals(redNoDetailInfo.getTaxRate())){
+                        isMixedRate = true;
+                    }
+                }
+            }
+        }
+
+        RedGeneratePdfDetailInfo combineEntity = new RedGeneratePdfDetailInfo();
+        combineEntity.setAmountWithoutTax(sumAmountWithoutTax.toPlainString());
+        combineEntity.setTaxAmount(sumTaxAmount.toPlainString());
+        combineEntity.setCargoName("详见对应正数发票及清单");
+//        combineEntity.setZeroTax("");
+        if(!isMixedRate){
+            combineEntity.setTaxRate(taxRate.toPlainString());
+        }
+        treatedRedNoDetails.clear();
+        return  combineEntity;
+    }
 
 
 
