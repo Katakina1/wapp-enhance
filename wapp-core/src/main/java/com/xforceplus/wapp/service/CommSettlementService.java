f385871143ee8bc3f2c5c8f4d31e3932fc0bb135
马龙 <malong@xforceplus.com>
2021-11-10 09:49:36 +0800

    fix reference cycle

--- wapp-core/src/main/java/com/xforceplus/wapp/service/CommSettlementService.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/service/CommSettlementService.java
@@ -17,6 +17,7 @@ import com.xforceplus.wapp.repository.entity.TXfPreInvoiceEntity;
 import com.xforceplus.wapp.repository.entity.TXfPreInvoiceItemEntity;
 import com.xforceplus.wapp.repository.entity.TXfSettlementEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
@@ -46,6 +47,7 @@ public class CommSettlementService {
     @Autowired
     private PreinvoiceService preinvoiceService;
     @Autowired
+    @Lazy
     private RedNotificationOuterService redNotificationOuterService;
     @Autowired
     private OperateLogService operateLogService;
