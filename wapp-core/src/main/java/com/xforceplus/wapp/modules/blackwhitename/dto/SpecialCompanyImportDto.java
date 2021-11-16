ab7398ea76eec349c0911060e1bd671c281266bc
AIWENTAO <aiwentao@xforceplus.com>
2021-11-16 11:17:28 +0800

    黑白名单拆分

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/blackwhitename/dto/SpecialCompanyImportDto.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/blackwhitename/dto/SpecialCompanyImportDto.java
@@ -11,8 +11,9 @@ import lombok.Data;
 @ApiModel("黑白名单导入配置")
 public class SpecialCompanyImportDto {
 
-    @ExcelProperty(value = "供应商6D编码", index = 0)
-    private String supplier6d;
+
+    @ExcelProperty(value = "供应商编号", index = 0)
+    private String sapNo;
 
 
     @ExcelProperty(value = "供应商名称", index = 1)
@@ -21,6 +22,9 @@ public class SpecialCompanyImportDto {
 
     @ExcelProperty(value = "供应商税号", index = 2)
     private String supplierTaxNo;
+
+    @ExcelProperty(value = "供应商6D编号", index = 3)
+    private String supplier6d;
 
     @ExcelProperty(index = 4)
     private String errorMessage;
