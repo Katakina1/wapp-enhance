bc38f324954f9cc909a200492974080cf40126a8
zhangzhenwei@xforeplus.com <Ab123456>
2021-11-17 14:41:14 +0800

    蓝票税率问题

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/deduct/service/DeductService.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/deduct/service/DeductService.java
@@ -976,7 +976,7 @@ public class DeductService   {
             BlueInvoiceService.InvoiceItem tmpInvoce = map.get(key);
             List<String> splitInfo = ItemNameUtils.splitItemName(key);
             if (splitInfo.size() != 2) {
-                res.put(key, null);
+                return;
             }
             val either = taxCodeService.searchTaxCode(String.valueOf(TaxRateTransferEnum.transferTaxRate(tmpInvoce.getTaxRate())), null, splitInfo.get(0));
             if (either.isRight()) {
@@ -984,11 +984,9 @@ public class DeductService   {
                 if (CollectionUtils.isNotEmpty(taxCodeBeans)) {
                     TaxCodeBean taxCodeBean = taxCodeBeans.get(0);
                     res.put(splitInfo.get(0), taxCodeBean);
-                 }else{
-                    res.put(splitInfo.get(0), null);
-                }
+                 }
             }
-            res.put(splitInfo.get(0), null);
+
         });
         return res;
     }
