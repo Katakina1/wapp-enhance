cee7ee0959f410a2e71ed54031b2f121eebe1c7e
sunshiyong@xforceplus.com <sunshiyong@xforceplus.com>
2021-12-01 15:16:04 +0800

    BIG-1257 : 换票发票列表详情

--- wapp-core/src/main/java/com/xforceplus/wapp/modules/exchange/model/InvoiceExchangeResponse.java
+++ wapp-core/src/main/java/com/xforceplus/wapp/modules/exchange/model/InvoiceExchangeResponse.java
@@ -17,98 +17,107 @@ public class InvoiceExchangeResponse {
     /**
      * 主键
      */
-    @ApiModelProperty("id")
+    @ApiModelProperty("主键")
     private Long id;
 
     /**
      * 底账发票id
      */
-    @ApiModelProperty("invoice_id")
+    @ApiModelProperty("底账发票id")
     private Long invoiceId;
 
     /**
      * 状态 0待换票 1已上传 2已完成 9删除
      */
-    @ApiModelProperty("status")
+    @ApiModelProperty("状态 0待换票 1已上传 2已完成 9删除")
     private Boolean status;
 
     /**
      * 创建时间
      */
-    @ApiModelProperty("create_time")
+    @ApiModelProperty("创建时间")
     private Date createTime;
 
     /**
      * orgcode
      */
-    @ApiModelProperty("jvcode")
+    @ApiModelProperty("orgcode")
     private String jvcode;
 
     /**
      * 供应商编码
      */
-    @ApiModelProperty("seller_no")
+    @ApiModelProperty("供应商编码")
     private String sellerNo;
 
     /**
      * 不含税金额
      */
-    @ApiModelProperty("amount_without_tax")
+    @ApiModelProperty("不含税金额")
     private BigDecimal amountWithoutTax;
 
     /**
      * 税额
      */
-    @ApiModelProperty("tax_amount")
+    @ApiModelProperty("税额")
     private BigDecimal taxAmount;
 
     /**
      * 业务类型
      */
-    @ApiModelProperty("business_type")
+    @ApiModelProperty("业务类型")
     private String businessType;
 
     /**
      * 退单号
      */
-    @ApiModelProperty("return_no")
+    @ApiModelProperty("退单号")
     private String returnNo;
 
     /**
      * 快递公司
      */
-    @ApiModelProperty("express_company")
+    @ApiModelProperty("快递公司")
     private String expressCompany;
 
     /**
      * 快递单号
      */
-    @ApiModelProperty("waybill_no")
+    @ApiModelProperty("快递单号")
     private String waybillNo;
 
     /**
      * 发票代码
      */
-    @ApiModelProperty("invoice_code")
+    @ApiModelProperty("发票代码")
     private String invoiceCode;
 
     /**
      * 发票号码
      */
-    @ApiModelProperty("invoice_no")
+    @ApiModelProperty("发票号码")
     private String invoiceNo;
 
     /**
      * 开票日期
      */
-    @ApiModelProperty("paper_drew_date")
+    @ApiModelProperty("开票日期")
     private String paperDrewDate;
 
     /**
      * 新开发票id，逗号隔开
      */
-    @ApiModelProperty("new_invoice_id")
+    @ApiModelProperty("新开发票id，逗号隔开")
     private String newInvoiceId;
+
+    @ApiModelProperty("换票原因")
+    private String remark;
+
+    @ApiModelProperty("发票类型")
+    private String invoiceType;
+
+    @ApiModelProperty("纸电类型：true电票 false纸票")
+    private boolean isElectronic;
 
 
 }
